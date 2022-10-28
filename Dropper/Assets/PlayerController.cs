14ed460d407688dd4cd8da752a3824da46836229
Luis Filipe Carrete <lfcsancehz@live.com>
2022-10-28 09:09:49 -0300

    Remake do proj

---
+++ Dropper/Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   public float _baseSpeed = 10.0f;
+   public float _gravidade = 9.8f;
+
+   //Referência usada para a câmera filha do jogador
+   GameObject playerCamera;
+   //Utilizada para poder travar a rotação no angulo que quisermos.
+   float cameraRotation;
+   CharacterController characterController;
+
+   void Start()
+   {
+        characterController = GetComponent<CharacterController>();
+        playerCamera = GameObject.Find("Main Camera");
+        cameraRotation = 0.0f;
+   }
+
+   void Update()
+   {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+       
+        //Verificando se é preciso aplicar a gravidade
+        float y = 0;
+        if(!characterController.isGrounded){
+            y = -_gravidade;
+        }
+       
+        Vector3 direction = transform.right * x + transform.up * y + transform.forward * z;
+
+        characterController.Move(direction * _baseSpeed * Time.deltaTime);
+
+       
+        //Tratando movimentação do mouse
+        float mouse_dX = Input.GetAxis("Mouse X");
+        float mouse_dY = Input.GetAxis("Mouse Y");
+       
+        //Tratando a rotação da câmera
+        cameraRotation -= mouse_dY;
+        cameraRotation = Mathf.Clamp(cameraRotation, -100f, 100f);
+
+        characterController.Move(direction * _baseSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up, mouse_dX);
+
+        playerCamera.transform.localRotation = Quaternion.Euler(cameraRotation, 0.0f, 0.0f);
+   }
+}
