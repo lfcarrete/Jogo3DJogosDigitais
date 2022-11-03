d2e09e6d895dc240f3cb2ddbf00f10f68516abc4
Luis Filipe Carrete <lfcsancehz@live.com>
2022-11-02 21:41:59 -0300

    .

--- Dropper/Assets/VictoryPlane.cs
+++ Dropper/Assets/VictoryPlane.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VictoryPlane : MonoBehaviour
 {
+    public string scenename;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -18,6 +21,6 @@ public class VictoryPlane : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        print("Hello");
+        SceneManager.LoadScene(scenename);
     }
 }
